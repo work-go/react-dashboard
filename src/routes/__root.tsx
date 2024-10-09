0472f16d4ee23cce28bd12dc29ddd2d9e6da0529
Anas Chebili <anaschebili@gmail.com>
2024-10-09 16:16:18 +0200

    confined tanstackrouter dev tools to the development environment

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -33,7 +33,7 @@ export const Route = createRootRoute({
       </div>
       <hr />
       <Outlet />
-      <TanStackRouterDevtools />
+      {process.env.NODE_ENV === "development" && <TanStackRouterDevtools />}
     </>
   ),
 });
