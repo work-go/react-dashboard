fb81cebdf055a68ecdf86e95fee121692a367996
Anas Chebili <anaschebili@gmail.com>
2024-10-21 16:45:27 +0200

    Implemented the login ans signup functionalities

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors:{
-        'theme-green':'#99fe00',
-      },
-      fontFamily:{
-        "roboto-regular":['Roboto-Regular', 'sans-serif'],
-        "roboto-bold":['Roboto-Bold', 'sans-serif'],
-        "roboto-black":['Roboto-black', 'sans-serif'],
-        "roboto-medium":['Roboto-Medium', 'sans-serif'],
-      }
-    },
+  	extend: {
+  		colors: {
+  			'theme-green': '#99fe00'
+  		},
+  		fontFamily: {
+  			'roboto-regular': ['Roboto-Regular', 'sans-serif'],
+  			'roboto-bold': ['Roboto-Bold', 'sans-serif'],
+  			'roboto-black': ['Roboto-black', 'sans-serif'],
+  			'roboto-medium': ['Roboto-Medium', 'sans-serif']
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
